980d70c5e9ebe7d8ae47e108f1bd8923c6346a06
Patrick Latorre <ptrckltrr@gmail.com>
2018-02-24 03:29:12 +0800

    + Added login windows

--- mpClientG.py
+++ mpClientG.py
@@ -3,16 +3,90 @@ import threading
 import time
 from tkinter import *
 import tkinter.messagebox
+import tkinter.simpledialog
 
 ''' 
 ================================
-GUI Object
+Classes
 ================================
 '''
-class client:
-	def __init__(self, master):
-		frame = Frame(master)
-		frame.pack()
+class Client:
+    name = ""
+    ip = ""
+    port = 0
+
+    def getName(self):
+        return self.name
+''' 
+================================
+Functions
+================================
+'''
+def foo():
+    tkinter.messagebox.showinfo(title="works", message="Placeholder.")
+
+
+def getNameIP():
+    name = ""
+    ip = ""
+    while name == "":
+        name = tkinter.simpledialog.askstring(title="PyChat 0.1", prompt="Username:")
+    while ip == "":
+        ip = tkinter.simpledialog.askstring(title="PyChat 0.1", prompt="Client IP Address:")
+    submitAnswer = tkinter.messagebox.showinfo(title="PyChat 0.1", message="Welcome " + name + "!")
+
+    return name, ip
+
+
+def getStarted():
+    loginScreen = Tk()
+    frame = Frame(loginScreen, width=200, height=64)
+    frame.pack()
+    loginLbl = Label(loginScreen, text="waiting for credentials..")
+    loginLbl.place(x=100, y=32, anchor=CENTER)
+    continueBtn = Button(loginScreen, text="continue", command=foo)
+
+    username = ""
+    userIP = ""
+    username, userIP = getNameIP()
+
+    loginScreen.destroy()
+
+    return username, userIP
+
+
+
+''' 
+================================
+GUI
+================================
+'''
+
+''' Login '''
+myName, myIP = getStarted()
+
+
+
+
+''' Main Screen '''
+mainScreen = Tk()
+
+menu = Menu(mainScreen)
+mainScreen.config(menu=menu)
+
+fileMenu = Menu(menu)
+menu.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="New Project", command=foo)
+fileMenu.add_command(label="New", command=foo)
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit", command=foo)
+
+frame = Frame(mainScreen, width=1280, height=720, bg="white")
+frame.pack()
+
+mainScreen.mainloop()
+
+
 
 
 ''' 
@@ -20,8 +94,10 @@ class client:
 Logic Handling
 ================================
 '''
+
 tlock = threading.Lock()
 shutdown = False
+
 
 # Receiving func
 def receving(name, sock):
@@ -36,63 +112,55 @@ def receving(name, sock):
         finally:
             tlock.release()
 
+
 host = '192.168.1.5'
 port = 0
 
-#Join a Server or Create
-chatDestination= input(" [1]Join Global Chat ")
+# Join a Server or Create
+chatDestination = input(" [1]Join Global Chat ")
 if chatDestination == '1':
-    #server = ('127.0.0.1', 5000)
     server = ('192.168.1.5', 5000)
-    
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host, port))
 s.setblocking(0)
 
-#Threading
+# Threading
 rT = threading.Thread(target=receving, args=("RecvThread", s))
 rT.start()
 
-#Chat Proper
+# Chat Proper
 alias = input("Name: ")
 message = input(alias + "-> ")
 while message != '/leave':
-    #You have to put /whiser next to your message to PM, ex. /whisper hello!
+    # You have to put /whiser next to your message to PM, ex. /whisper hello!
     if "/whisper" in message:
-        pmhostnum=(input("Enter Host Name:"))
-        pmportnum=int(input("Enter Port Number:"))
-        privateserver=(pmhostnum,pmportnum)
+        pmhostnum = (input("Enter Host Name:"))
+        pmportnum = int(input("Enter Port Number:"))
+        privateserver = (pmhostnum, pmportnum)
         pm = alias + ": " + message
-        pm= pm.encode('utf-8') 
+        pm = pm.encode('utf-8')
         s.sendto(pm, privateserver)
-    
+
     elif message == '/members':
         m = alias + ": " + message
-        m= m.encode('utf-8')
+        m = m.encode('utf-8')
         s.sendto(m, server)
-        
+
     elif message != '':
         b = alias + ": " + message
-        b= b.encode('utf-8')
+        b = b.encode('utf-8')
         s.sendto(b, server)
-    
-    
-    
-    
+
     tlock.acquire()
     message = input(alias + "-> ")
     tlock.release()
     time.sleep(0.2)
-    
-leave=alias+ "left" 
-leave= leave.encode('utf-8')   
+
+leave = alias + "left"
+leave = leave.encode('utf-8')
 s.sendto(leave, server)
-print(alias +" left ")    
+print(alias + " left ")
 shutdown = True
 rT.join()
 s.close()
-
-
-
